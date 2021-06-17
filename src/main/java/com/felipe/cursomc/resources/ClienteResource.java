69293b8d756feaf4d4f2e2b473087df19934820f
Felipe Bruno <felipesvascon@gmail.com>
2021-06-17 20:15:32 -0300

    Endpoint /cliente{id} disponivel

---
+++ src/main/java/com/felipe/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.felipe.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.felipe.cursomc.domain.Cliente;
+import com.felipe.cursomc.services.ClienteService;
+
+@Controller
+@RequestMapping(value = "/cliente")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/felipe/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.felipe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.cursomc.domain.Cliente;
+import com.felipe.cursomc.repositories.ClienteRepository;
+import com.felipe.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: "+ id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
