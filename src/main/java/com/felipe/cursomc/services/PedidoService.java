743e3a278c5fa0c4d6df1c61ee2a0b6b59ed8650
Felipe Bruno <felipesvascon@gmail.com>
2021-06-21 12:09:49 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/felipe/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.felipe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.cursomc.domain.Pedido;
+import com.felipe.cursomc.repositories.PedidoRepository;
+import com.felipe.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
